9a0637413df7210974a3331f4d5dc7925b6a34a7
brandonmontell <brandonmontell@gmail.com>
2013-06-12 23:38:47 -0700

    MERGING OMG THAT TOOK FOREVER

---
+++ PollDanceProject/PollDance/src/main/gen/com/grahamp1/polldance/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package com.grahamp1.polldance;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ PollDanceProject/PollDance/src/main/java/com/grahamp1/polldance/ResultsActivity.java
@@ -0,0 +1,18 @@
+package com.grahamp1.polldance;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by demouser on 6/12/13.
+ */
+public class ResultsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_host_home);
+
+    }
+
+}
