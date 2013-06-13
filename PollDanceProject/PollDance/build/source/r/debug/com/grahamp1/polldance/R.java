54ec136051d3acd7ae72c07a979749523e8b2269
brandonmontell <brandonmontell@gmail.com>
2013-06-12 22:37:38 -0700

    GUI stuff

--- PollDanceProject/PollDance/build/source/r/debug/com/grahamp1/polldance/R.java
+++ PollDanceProject/PollDance/build/source/r/debug/com/grahamp1/polldance/R.java
@@ -18,16 +18,24 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int create_poll_button=0x7f080001;
-        public static final int host_button=0x7f080003;
-        public static final int load_poll_button=0x7f080002;
+        public static final int action_settings=0x7f08000a;
+        public static final int create_answer_button=0x7f080003;
+        public static final int create_answer_list=0x7f080004;
+        public static final int create_poll_button=0x7f080006;
+        public static final int create_question_text=0x7f080001;
+        public static final int host_button=0x7f080008;
+        public static final int load_poll_button=0x7f080007;
+        public static final int new_answer_text=0x7f080009;
+        public static final int share_poll_button=0x7f080005;
         public static final int textView=0x7f080000;
+        public static final int textView2=0x7f080002;
     }
     public static final class layout {
         public static final int activity_create_poll=0x7f030000;
         public static final int activity_host_home=0x7f030001;
         public static final int activity_main=0x7f030002;
+        public static final int activity_results=0x7f030003;
+        public static final int fragment_new_answer=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- PollDanceProject/PollDance/src/main/java/com/grahamp1/polldance/CreatePollActivity.java
+++ PollDanceProject/PollDance/src/main/java/com/grahamp1/polldance/CreatePollActivity.java
@@ -2,18 +2,53 @@ package com.grahamp1.polldance;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by demouser on 6/12/13.
  */
 public class CreatePollActivity extends Activity {
 
+    ArrayList<String> _listItems = new ArrayList<String>();
+    ArrayAdapter<String> _adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_poll);
 
+        findViewById(R.id.create_answer_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new NewAnswerDialogFragment().show(getFragmentManager(), "dialog");
+            }
+        });
 
+        findViewById(R.id.share_poll_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String questionText = ((EditText) findViewById(R.id.create_question_text))
+                        .getText().toString();
+            }
+        });
+
+        _adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, _listItems);
+        ListView lv = (ListView) findViewById(R.id.create_answer_list);
+        lv.setAdapter(_adapter);
+
+        lv.setClickable(true);
+//        lv.setOnI
+    }
+
+    public void createNewAnswer(String answerText) {
+        _listItems.add(answerText);
+        _adapter.notifyDataSetChanged();
     }
 
 }
